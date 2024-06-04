36f0bb53c56d94d05ab3f247d195d38353ac9982
HagOrMan <khagypi3@gmail.com>
2024-06-04 19:04:01 -0400

    feat: added dept-and-program-reps page

---
+++ src/app/dept-and-program-reps/page.tsx
@@ -0,0 +1,29 @@
+import CouncilProfile from '@/components/council/CouncilProfile';
+import PageLayout from '@/components/layout/PageLayout';
+import PageHeading from '@/components/PageHeading';
+
+import deptAndProgramReps from '@/constant/council/departmentAndProgramReps';
+
+export default function DeptProgramRepsPage() {
+  return (
+    <PageLayout>
+      <main className='layout'>
+        <section>
+          <PageHeading
+            preTitle='2023-2024 Team'
+            title='Department & Program Reps'
+            variant='red'
+          />
+          <div id='executives-list' className='mt-8 space-y-8'>
+            {deptAndProgramReps.map((rep) => (
+              <CouncilProfile
+                key={`${rep.firstName}-${rep.lastName}`}
+                profile={rep}
+              />
+            ))}
+          </div>
+        </section>
+      </main>
+    </PageLayout>
+  );
+}
