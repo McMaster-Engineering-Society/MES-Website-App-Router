e5ff3900bf1d4d23deed5904edefbb48e79760db
HagOrMan <khagypi3@gmail.com>
2024-06-16 21:16:27 -0400

    feat: added mongo db connector

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,16 +1,4 @@
 // This approach is taken from https://github.com/vercel/next.js/tree/canary/examples/with-mongodb
-/**
- * To use the client promise in another file, use the following code:
- *     await clientPromise;
-    // `await clientPromise` will use the default database passed in the MONGODB_URI
-    // However you can use another database (e.g. myDatabase) by replacing the `await clientPromise` with the following code:
-    //
-    // `const client = await clientPromise`
-    // `const db = client.db("myDatabase")`
-    //
-    // Then you can execute queries against your database like so:
-    // db.find({}) or any of the MongoDB Node Driver commands
- */
 import { MongoClient, ServerApiVersion } from 'mongodb';
 
 if (!process.env.MONGODB_URI) {
