b48bd3a306d8f2d6cbd2067a6f3a9e39cd0257a5
HagOrMan <khagypi3@gmail.com>
2024-06-16 23:54:35 -0400

    feat: added email helpers to customize email signin

--- auth.ts
+++ auth.ts
@@ -1,8 +1,19 @@
 import { MongoDBAdapter } from '@auth/mongodb-adapter';
 import NextAuth from 'next-auth';
 import NodeMailer from 'next-auth/providers/nodemailer';
+import { createTransport } from 'nodemailer';
 
 import clientPromise from '@/lib/db';
+import { html, text } from '@/lib/emailHelper';
+
+// Checks if the domain name is mcmaster
+function isMacEmail(email: string) {
+  /**
+   * Checks if the given email is from a mcmaster domain
+   */
+  const [_, domain] = email.toLowerCase().trim().split('@');
+  return domain == 'mcmaster.ca';
+}
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
@@ -16,7 +27,46 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         },
       },
       from: process.env.EMAIL_FROM,
+      normalizeIdentifier(identifier: string): string {
+        // Throw an error if not Mac email
+        if (!isMacEmail(identifier)) {
+          // throws error which will redirect the user to the sign-in page with error=EmailSignin in the URL
+          throw new Error('Must use a McMaster email address to sign in.');
+        }
+
+        // Default behaviour for this function
+        // eslint-disable-next-line prefer-const
+        let [local, domain] = identifier.toLowerCase().trim().split('@');
+        domain = domain.split(',')[0];
+        return `${local}@${domain}`;
+      },
+      async sendVerificationRequest({
+        identifier,
+        url,
+        provider: { server, from },
+      }: {
+        identifier: string;
+        url: string;
+        provider: { server: string; from: string };
+      }) {
+        const { host } = new URL(url);
+        const transport = createTransport(server);
+        const result = await transport.sendMail({
+          to: identifier,
+          from: from,
+          subject: `Sign in to ${host}`,
+          text: text({ url, host }),
+          html: html({ url, host }),
+        });
+        const failed = result.rejected.concat(result.pending).filter(Boolean);
+        if (failed.length) {
+          throw new Error(`Email(s) (${failed.join(', ')}) could not be sent`);
+        }
+      },
     }),
   ],
   adapter: MongoDBAdapter(clientPromise),
+  pages: {
+    error: '/auth/error',
+  },
 });
