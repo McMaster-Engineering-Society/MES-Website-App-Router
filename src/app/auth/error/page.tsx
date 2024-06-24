9da6e0b2a14d71824f5c6effa73ecfb82e0ce0ba
HagOrMan <khagypi3@gmail.com>
2024-06-24 09:49:11 -0400

    feat: mac email still disabled, but added more to error page

--- src/app/auth/error/page.tsx
+++ src/app/auth/error/page.tsx
@@ -1,44 +1,19 @@
-'use client';
-
-import { useSearchParams } from 'next/navigation';
 import { redirect } from 'next/navigation';
-import * as React from 'react';
-import { RiAlarmWarningFill } from 'react-icons/ri';
+import React from 'react';
 
-import PageLayout from '@/components/layout/PageLayout';
-import ButtonLink from '@/components/links/ButtonLink';
+import { checkIsAuthenticated } from '@/lib/auth/emailSignInHelper';
 
-// TODO: reformat page, make prettier
-export default function AuthError() {
-  const searchParams = useSearchParams();
-  const error = searchParams.get('error');
-  // Exits the page if we didn't get an error to lead us here from NextAuth.
-  if (!error) {
-    redirect('/auth/sign-in');
+import { ErrorPage } from '@/app/auth/error/ErrorPage';
+
+const Error = async () => {
+  const isAuthenticated = await checkIsAuthenticated();
+
+  // Redirects to home page if for some reason someone tries to access the error page but they're signed in.
+  if (isAuthenticated) {
+    redirect('/');
+  } else {
+    return <ErrorPage />;
   }
-  return (
-    <PageLayout>
-      <main className='layout'>
-        <section className='bg-white'>
-          <div className='layout flex min-h-screen flex-col items-center justify-center text-center text-black'>
-            <RiAlarmWarningFill
-              size={60}
-              className='drop-shadow-glow animate-flicker text-red-500'
-            />
-            <h1 className='mt-8 text-4xl md:text-6xl'>
-              There was an error with signing in. You must use a McMaster email
-              address in order to sign in.
-            </h1>
-            <ButtonLink
-              href='/auth/sign-in'
-              variant='outline'
-              className='mt-8 w-1/4'
-            >
-              Try Again
-            </ButtonLink>
-          </div>
-        </section>
-      </main>
-    </PageLayout>
-  );
-}
+};
+
+export default Error;
