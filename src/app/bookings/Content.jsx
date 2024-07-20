c1d1a91cebbcea5813ca812c027b155e7da578c9
shivyam <144865023+shivyam@users.noreply.github.com>
2024-07-20 18:52:43 -0400

    fix: cleaned up code + added variables

--- src/app/bookings/Content.jsx
+++ src/app/bookings/Content.jsx
@@ -1,16 +1,17 @@
-
-'use client'
 import React from "react";
+
 import AvailableRoom from '@/components/bookings/AvailableRoom';
+import {AvailableRooms} from "@/constant/hatch-bookings/available-rooms";
 
 export default function Content() {
 
   return(
     <>
+    {AvailableRooms.map((roomInfo) => (<AvailableRoom key={roomInfo.roomNum} {...roomInfo}/> ))}
+    {/* <AvailableRoom/>
     <AvailableRoom/>
     <AvailableRoom/>
-    <AvailableRoom/>
-    <AvailableRoom/>
+    <AvailableRoom/> */}
   
     </>
   );
--- src/components/bookings/AvailableRoom.tsx
+++ src/components/bookings/AvailableRoom.tsx
@@ -1,15 +1,16 @@
 
 'use client'
+import {Button, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, useDisclosure} from "@nextui-org/react";
 import React from "react";
-import {Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure} from "@nextui-org/react";
 
+import { HatchRoomType } from "@/constant/hatch-bookings/available-rooms";
 
-export default function Content() {
+export default function AvailableRoom(roomInfo: HatchRoomType) {
   const {isOpen, onOpen, onOpenChange} = useDisclosure();
   return(
     <>
       <div className="bg-[#A6192E] box-border rounded-xl h-36 w-40 p-4 my-4 border-4 text-center flex flex-col items-center justify-center"> 
-        <p><b>204A</b></p> 
+        <p><b>{roomInfo.roomNum}</b></p> 
         <Button className="bg-white box-border rounded-xl h-20 w-26 p-1 my-2 border-4  text-center items-center" onPress={onOpen}>
           <b>Room Information</b>
         </Button>
@@ -19,11 +20,13 @@ export default function Content() {
         <ModalContent>
           {(onClose) => (
             <>
-              <ModalHeader className="flex flex-col gap-1">204A</ModalHeader>
+              <ModalHeader className="flex flex-col gap-1">{roomInfo.roomNum}</ModalHeader>
               <ModalBody>
                 <p> 
-                  Room capacity: 2
-                  Outlet: 4
+                  Room capacity: {roomInfo.capacity}
+                </p>
+                <p>
+                  Outlet: {roomInfo.outlets}
                 </p>
                 <p>
                   Resources: TV, Whiteboard
