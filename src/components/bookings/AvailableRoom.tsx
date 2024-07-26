2add513dd0107bd5046879f17c662e79720ed193
shivyam <144865023+shivyam@users.noreply.github.com>
2024-07-26 00:12:45 -0400

    fix: updated available rooms component to display available reosurces on card

--- src/components/bookings/AvailableRoom.tsx
+++ src/components/bookings/AvailableRoom.tsx
@@ -8,24 +8,49 @@ import {
   ModalHeader,
   useDisclosure,
 } from '@nextui-org/react';
+import { InfoIcon } from 'lucide-react';
 import Image from 'next/image';
 import React from 'react';
 
 import { HatchRoomType } from '@/constant/hatch-bookings/available-rooms';
 
+import { CheckmarkIcon } from '../../../public/svg/checkmark';
+
 export default function AvailableRoom(roomInfo: HatchRoomType) {
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
   return (
     <>
-      <div className='bg-[#A6192E] text-white box-border rounded-xl h-36 w-40 p-4 my-4 border-4 text-center flex flex-col items-center justify-center'>
-        <p>
-          <b>{roomInfo.roomNum}</b>
-        </p>
+      <div className='bg-[#A6192E] text-white box-border rounded-xl w-40 p-4 my-4 border-4 text-center flex flex-col items-center'>
+        <div className='relative inline-block w-full text-center justify-center items-center pb-1'>
+          <div className='inline-block font-bold'>{roomInfo.roomNum}</div>
+          <button
+            className='absolute right-0 top-0.5 hover:bg-red-950 rounded-full'
+            onClick={onOpen}
+          >
+            <InfoIcon size={20} />
+          </button>
+        </div>
+        <div className='w-full text-left font-semibold'>
+          <p>
+            Capacity: {roomInfo.capacity} <br />{' '}
+          </p>
+          <p>
+            Outlets: {roomInfo.outlets} <br />{' '}
+          </p>
+
+          {roomInfo.resources.map((resource, index) => (
+            <div className='flex' key={index}>
+              <CheckmarkIcon /> {resource}
+            </div>
+          ))}
+        </div>
         <Button
-          className='bg-white box-border rounded-xl h-20 w-26 p-1 my-2 border-4  text-center items-center'
-          onPress={onOpen}
+          className='m-1 font-semibold bg-[#FFDEA7]'
+          size='sm'
+          color='success'
         >
-          <b>Room Information</b>
+          Book Now
         </Button>
       </div>
 
@@ -45,7 +70,12 @@ export default function AvailableRoom(roomInfo: HatchRoomType) {
               <ModalBody>
                 <p>Room capacity: {roomInfo.capacity}</p>
                 <p>Outlet: {roomInfo.outlets}</p>
-                <p>Resources: TV, Whiteboard</p>
+                <p>
+                  Resources:{' '}
+                  {roomInfo.resources.map((resource) => {
+                    return resource;
+                  })}
+                </p>
                 <Image
                   src={roomInfo.img}
                   width={200}
@@ -54,6 +84,12 @@ export default function AvailableRoom(roomInfo: HatchRoomType) {
                 />
               </ModalBody>
               <ModalFooter>
+                <Button
+                  className='m-1 font-semibold bg-[#FFDEA7]'
+                  color='success'
+                >
+                  Book Now
+                </Button>
                 <Button color='danger' variant='light' onPress={onClose}>
                   Close
                 </Button>
