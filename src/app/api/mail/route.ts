1421ad3d147c1ce782de9ac26fa3edb491cfccd4
Aidan Lao <laoaidan@gmail.com>
2024-08-04 20:44:06 -0300

    fix: fixed pnpm run typecheck errors

--- src/app/api/mail/route.ts
+++ src/app/api/mail/route.ts
@@ -1,15 +1,16 @@
 import { NextRequest, NextResponse } from 'next/server';
 import nodemailer from 'nodemailer';
-
-const transporter = nodemailer.createTransport({
+import SMTPTransport from 'nodemailer/lib/smtp-transport';
+const options: SMTPTransport.Options = {
   host: process.env.SMTP_SERVER,
-  port: process.env.SMTP_PORT,
+  port: parseInt(process.env.SMTP_PORT as string),
   secure: process.env.SMTP_PORT == '465' ? true : false, // Use `true` for port 465, `false` for all other ports
   auth: {
     user: process.env.SMTP_USER, // your email address
     pass: process.env.SMTP_PASS, // your email password
   },
-});
+};
+const transporter = nodemailer.createTransport(options);
 
 export async function POST(req: NextRequest) {
   const { to, subject, text } = await req.json();
@@ -18,7 +19,7 @@ export async function POST(req: NextRequest) {
     // Send email
     const info = await transporter.sendMail({
       from: `${process.env.EMAIL_FROM}`,
-      to: to || `${process.env.EMAIL_FROM}`,
+      to,
       subject,
       text,
       // html: "<b>Hello world?</b>", // html body if we want it in the future
