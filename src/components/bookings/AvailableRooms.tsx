c5becfd5148caafa1a5651550621283287f0766a
Kyle <86536365+HagOrMan@users.noreply.github.com>
2024-08-15 09:42:36 -0400

    Revert "Revert "Feature/available rooms""

---
+++ src/components/bookings/AvailableRooms.tsx
@@ -0,0 +1,28 @@
+import { ScrollShadow } from '@nextui-org/react';
+import React from 'react';
+
+import AvailableRoom from '@/components/bookings/AvailableRoom';
+
+import { AvailableRoomsData } from '@/constant/hatch-bookings/available-rooms';
+
+type ContentProps = {
+  availableRoomIds: string[];
+};
+
+export default function AvailableRooms({ availableRoomIds }: ContentProps) {
+  return (
+    <ScrollShadow
+      hideScrollBar
+      className='flex justify-center w-[200px] h-[450px] mt-8 rounded-lg bg-[#CACDD1]'
+    >
+      <div>
+        {/* Displays only the available rooms for the selected timeslot */}
+        {AvailableRoomsData.filter((room) => {
+          return availableRoomIds.includes(room.roomNum);
+        }).map((roomInfo) => (
+          <AvailableRoom key={roomInfo.roomNum} {...roomInfo} />
+        ))}
+      </div>
+    </ScrollShadow>
+  );
+}
