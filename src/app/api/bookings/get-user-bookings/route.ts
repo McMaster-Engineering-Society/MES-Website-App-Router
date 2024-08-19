dca46098feb5050563a9830e39bf8b8e025a5b2b
BaoGeist <linofbaoze@outlook.com>
2024-08-18 23:30:34 -0400

    feat: add get user bookings endpoint

--- src/app/api/bookings/get-user-bookings/route.ts
+++ src/app/api/bookings/get-user-bookings/route.ts
@@ -1,1 +1,27 @@
-export {};
+import { NextRequest, NextResponse } from 'next/server';
+
+import { getBookingsByUserIdService } from '@/lib/services/bookingServices';
+
+import { TApiResponse, TBooking, TMessageResponse } from '@/app/api/types';
+
+export async function GET(req: NextRequest) {
+  const searchParams = req.nextUrl.searchParams;
+  const userId = searchParams.get('userId');
+
+  if (!userId) {
+    return NextResponse.json<TMessageResponse>({
+      message: 'Invalid query params, missing either start date or end date.',
+    });
+  }
+
+  const userBookingsWithinTimeframeList =
+    await getBookingsByUserIdService(userId);
+
+  if (!userBookingsWithinTimeframeList)
+    return NextResponse.json<TMessageResponse>({
+      message: 'List of all bookings from this user not found',
+    });
+  return NextResponse.json<TApiResponse<TBooking[]>>({
+    data: userBookingsWithinTimeframeList,
+  });
+}
