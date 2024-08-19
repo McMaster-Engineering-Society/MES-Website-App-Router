c1b0e6d13afca15e5141d3762f20489bbd50368b
HagOrMan <khagypi3@gmail.com>
2024-08-19 09:37:08 -0400

    fix: invalid DOM property names

--- src/app/hatch-booking/page.tsx
+++ src/app/hatch-booking/page.tsx
@@ -40,9 +40,9 @@ export default function NewBookingSystemPage() {
                     stroke='currentColor'
                   >
                     <path
-                      stroke-linecap='round'
-                      stroke-linejoin='round'
-                      stroke-width='2'
+                      strokeLinecap='round'
+                      strokeLinejoin='round'
+                      strokeWidth='2'
                       d='M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z'
                     />{' '}
                   </svg>
