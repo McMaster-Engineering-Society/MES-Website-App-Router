52c28cc84a0ca96e3af45d13c66f5ee721aeaf0d
shivyam <144865023+shivyam@users.noreply.github.com>
2024-08-19 21:59:54 -0400

    fix: removed popover from modal, updated styling

--- src/components/bookings/AvailableRooms.tsx
+++ src/components/bookings/AvailableRooms.tsx
@@ -9,9 +9,18 @@ import { HatchRoomsData } from '@/constant/hatch-bookings/rooms-data';
 
 export default function AvailableRooms() {
   const { availableRoomIds } = useTimePickerContext();
+  const selected = true;
+  const numAvailRooms = 2;
 
   return (
-    <div className='flex justify-center w-[200px] h-[500px] my-8 rounded-lg bg-[#CACDD1]'>
+    <div className='flex flex-col justify-center items-center w-[200px] h-[500px] my-4 rounded-lg bg-[#CACDD1]'>
+      <div className='flex justify-center items-center text-center font-bold mt-4'>
+        {numAvailRooms > 0
+          ? selected
+            ? 'Available Rooms'
+            : 'Select time to view available rooms'
+          : 'No Rooms Available'}
+      </div>
       <ScrollShadow hideScrollBar>
         {/* Displays only the available rooms for the selected timeslot */}
         {HatchRoomsData.filter((room) =>
