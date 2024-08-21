0a9b242ba18e00d54e95e8597daca6debaa72031
shivyam <144865023+shivyam@users.noreply.github.com>
2024-08-20 23:07:52 -0400

    fix: rebased, adjusted component height, works with time picker

--- src/components/bookings/AvailableRooms.tsx
+++ src/components/bookings/AvailableRooms.tsx
@@ -2,33 +2,39 @@ import { ScrollShadow } from '@nextui-org/react';
 import React from 'react';
 
 import { useTimePickerContext } from '@/lib/context/TimePickerContext';
+import { TimePickerProvider } from '@/lib/context/TimePickerContext';
 
 import { AvailableRoomCard } from '@/components/bookings/AvailableRoomCard';
 
 import { HatchRoomsData } from '@/constant/hatch-bookings/rooms-data';
 
 export default function AvailableRooms() {
-  const { availableRoomIds } = useTimePickerContext();
-  const selected = true;
-  const numAvailRooms = 2;
+  const { availableRoomIds, startTimeDate } = useTimePickerContext();
+
+  const numAvailRooms = availableRoomIds.length;
 
   return (
-    <div className='flex flex-col justify-center items-center w-[200px] h-[500px] my-4 rounded-lg bg-[#CACDD1]'>
-      <div className='flex justify-center items-center text-center font-bold mt-4'>
-        {numAvailRooms > 0
-          ? selected
-            ? 'Available Rooms'
-            : 'Select time to view available rooms'
-          : 'No Rooms Available'}
+    <TimePickerProvider>
+      <div className='flex flex-col'>
+        <div className=' w-[200px] p-2 bg-[#CACDD1] font-bold text-center rounded-lg items-center'>
+          Available Rooms
+        </div>
+        <div className='h-[565px] flex flex-col justify-center items-center w-[200px]  rounded-lg bg-[#CACDD1] mt-2 p-2'>
+          <div className='flex justify-center items-center text-center font-bold'>
+            {startTimeDate
+              ? numAvailRooms == 0 && 'No Rooms Available'
+              : 'Click/drag to select a time'}
+          </div>
+          <ScrollShadow hideScrollBar>
+            {/* Displays only the available rooms for the selected timeslot */}
+            {HatchRoomsData.filter((room) =>
+              availableRoomIds.includes(room.roomName),
+            ).map((roomInfo) => (
+              <AvailableRoomCard key={roomInfo.roomName} roomInfo={roomInfo} />
+            ))}
+          </ScrollShadow>
+        </div>
       </div>
-      <ScrollShadow hideScrollBar>
-        {/* Displays only the available rooms for the selected timeslot */}
-        {HatchRoomsData.filter((room) =>
-          availableRoomIds.includes(room.roomName),
-        ).map((roomInfo) => (
-          <AvailableRoomCard key={roomInfo.roomName} roomInfo={roomInfo} />
-        ))}
-      </ScrollShadow>
-    </div>
+    </TimePickerProvider>
   );
 }
