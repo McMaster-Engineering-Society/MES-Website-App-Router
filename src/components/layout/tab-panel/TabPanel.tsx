89a5f7474f1087dfe49c923e218b895c13512671
Ethan Patterson <pattee14@mcmaster.ca>
2024-08-27 21:54:13 -0400

    fix: stutter on initial tab panel load

--- src/components/layout/tab-panel/TabPanel.tsx
+++ src/components/layout/tab-panel/TabPanel.tsx
@@ -2,17 +2,13 @@ import { TabPanel as MuiTabPanel, TabPanelProps } from '@mui/lab';
 import React from 'react';
 
 const TabPanel: React.FC<TabPanelProps> = (props) => {
-  return (
-    <MuiTabPanel
-      sx={{
-        flexBasis: '100%',
-        overflow: 'hidden',
-        display: 'flex',
-        padding: 0,
-      }}
-      {...props}
-    />
-  );
+  const sx = {
+    overflow: 'hidden',
+    display: 'flex',
+    padding: 0,
+    ...props.sx,
+  };
+  return <MuiTabPanel {...props} sx={sx} />;
 };
 
 export default TabPanel;
