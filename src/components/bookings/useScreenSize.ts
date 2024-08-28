ca5e4e3ba6ce1222e5eaa05f0ebaa1bd9f975165
Owen Gretzinger <owengretzinger@gmail.com>
2024-08-28 14:30:44 -0400

    fix: fix useScreenSize

--- src/components/bookings/useScreenSize.ts
+++ src/components/bookings/useScreenSize.ts
@@ -5,9 +5,9 @@ export function useScreenSize() {
 
   useEffect(() => {
     function handleResize() {
-      if (window.innerWidth > 1024) setScreenSize('lg');
-      else if (window.innerWidth > 768) setScreenSize('md');
-      else if (window.innerWidth > 640) setScreenSize('sm');
+      if (window.innerWidth >= 1024) setScreenSize('lg');
+      else if (window.innerWidth >= 768) setScreenSize('md');
+      else if (window.innerWidth >= 640) setScreenSize('sm');
       else setScreenSize('xs');
     }
 
