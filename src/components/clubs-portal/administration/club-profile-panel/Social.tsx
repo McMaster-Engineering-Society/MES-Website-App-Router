37d61ed27974b79931ccc18632338a8aaa228010
Ethan Patterson <pattee14@mcmaster.ca>
2024-08-28 00:11:24 -0400

    feat: add social media button

--- src/components/clubs-portal/administration/club-profile-panel/Social.tsx
+++ src/components/clubs-portal/administration/club-profile-panel/Social.tsx
@@ -2,11 +2,19 @@
 
 import { TextField } from '@mui/material';
 import React from 'react';
-import { FaInstagramSquare } from 'react-icons/fa';
-import { FaDiscord } from 'react-icons/fa';
-import { FaLinkedin } from 'react-icons/fa';
-import { FaGlobe } from 'react-icons/fa';
-import { FaFacebook } from 'react-icons/fa';
+import {
+  FaDiscord,
+  FaFacebook,
+  FaGithub,
+  FaGlobe,
+  FaInstagramSquare,
+  FaLinkedin,
+  FaMailBulk,
+  FaTiktok,
+  FaTree,
+  FaTwitter,
+  FaYoutube,
+} from 'react-icons/fa';
 import { IconType } from 'react-icons/lib';
 
 import Box from '@/components/clubs-portal/administration/club-profile-panel/Box';
@@ -24,7 +32,7 @@ const Social = ({ name, value, onChange }: SocialProps) => {
   return (
     <div className='flex flex-row items-center w-full'>
       <Icon className='mr-2' size='40' />
-      <Box className='w-full pb-3'>
+      <Box className='w-full'>
         <TextField
           fullWidth
           value={value}
@@ -43,10 +51,10 @@ export const socialIcons: Record<SocialMedia, IconType> = {
   linkedin: FaLinkedin,
   facebook: FaFacebook,
   website: FaGlobe,
-  github: FaGlobe, //place holder globes
-  twitter: FaGlobe,
-  youtube: FaGlobe,
-  linktree: FaGlobe,
-  tiktok: FaGlobe,
-  mailingList: FaGlobe,
+  github: FaGithub,
+  twitter: FaTwitter,
+  youtube: FaYoutube,
+  linktree: FaTree, //update
+  tiktok: FaTiktok,
+  mailingList: FaMailBulk, //update
 };
