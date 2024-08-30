ff62acbf14edad4d1a938633b3de9ad9076b0dbb
Seaya Liu <seayaliu7@gmail.com>
2024-08-30 01:41:17 -0400

    fix: update page and modals

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,321 +1,128 @@
 'use client';
-import { Button } from '@nextui-org/react';
-import { motion } from 'framer-motion';
-import Link from 'next/link';
-import { useEffect, useState } from 'react';
-import { IconType } from 'react-icons';
-import {
-  IoChatbubblesOutline,
-  IoDocumentTextOutline,
-  IoGlobeOutline,
-  IoNewspaperOutline,
-  IoPeopleOutline,
-} from 'react-icons/io5';
-import '@mui/material/styles';
+import { MessageCircleWarningIcon } from 'lucide-react';
+import { MailIcon } from 'lucide-react';
+import { NotebookPenIcon } from 'lucide-react';
+import { ChevronLeftIcon } from 'lucide-react';
+import { useState } from 'react';
 
-import Calendar from '@/components/calendar/Calendar';
-import HeroSectionColumn from '@/components/home/HeroSectionColumn';
 import PageLayout from '@/components/layout/PageLayout';
-import ArrowLink from '@/components/links/ArrowLink';
+import ButtonLink from '@/components/links/ButtonLink';
+import RebookModal from '@/components/modals/RebookModal';
+import ActionCard from '@/components/user-dashboard/ActionCard';
+import DashboardIconSvg from '@/components/user-dashboard/DashboardIconSvg';
+import NextBooking from '@/components/user-dashboard/NextBooking';
+import UpcomingBookings from '@/components/user-dashboard/UpcomingBookings';
+import UserInfo from '@/components/user-dashboard/UserInfo';
 
-import { councilForms } from '@/constant/council-resources/councilForms';
-import { electionInfo } from '@/constant/elections/electionInfo';
-import {
-  containerMotionVariants,
-  itemMotionVariants,
-} from '@/constant/home/animations';
+const UserDashboard = () => {
+  const [open, setOpen] = useState<boolean>(false);
 
-import { CalendarId } from '@/types/calendar';
-
-const API_KEY = 'AIzaSyDg60WsfHReUpNIDTD1KwI0UDDYQP02Yng';
-const CALENDAR_IDS: CalendarId[] = [
-  {
-    name: 'MES Events',
-    id: 'macengsociety.ca_1287bevihgvppm0boumra7eb6c@group.calendar.google.com',
-  },
-];
-const quickLinks: { icon: IconType; label: string; href: string }[] = [
-  {
-    icon: IoChatbubblesOutline,
-    label: 'Attend Conferences',
-    href: '/conferences',
-  },
-  {
-    icon: IoPeopleOutline,
-    label: 'Affiliated Clubs & Teams',
-    href: '/clubs-and-teams',
-  },
-  {
-    icon: IoDocumentTextOutline,
-    label: 'Council Resources',
-    href: '/council-resources',
-  },
-  {
-    icon: IoNewspaperOutline,
-    label: 'Advertisement Request',
-    href: councilForms[0].forms[0].link,
-  },
-  {
-    icon: IoGlobeOutline,
-    label: 'Web Update Request',
-    href: councilForms[0].forms[1].link,
-  },
-];
-
-export default function HomePage() {
-  const [today, setToday] = useState<Date>(new Date());
-
-  const [electionStatus, setElectionStatus] = useState<string>('');
-
-  useEffect(() => {
-    setToday(new Date());
-  }, []);
-
-  useEffect(() => {
-    let status: string;
-    if (today < electionInfo.nominationsClose) {
-      status = 'Run for candidacy';
-    } else if (
-      today >= electionInfo.nominationsClose &&
-      today < electionInfo.campaignsOpen
-    ) {
-      const timeUntilCampaignsOpen =
-        (electionInfo.campaignsOpen.getTime() - today.getTime()) /
-        (1000 * 60 * 60 * 24);
-
-      status = `Campaigns open in ${timeUntilCampaignsOpen.toFixed(0)} day${
-        timeUntilCampaignsOpen.toFixed(0) !== '1' ? 's' : ''
-      }`;
-    } else if (
-      today >= electionInfo.campaignsOpen &&
-      today < electionInfo.campaignsClose
-    ) {
-      status = 'Check out the candidates';
-    } else if (
-      today >= electionInfo.campaignsClose &&
-      today < electionInfo.votingOpen
-    ) {
-      const timeUntilVotingOpen =
-        (electionInfo.votingOpen.getTime() - today.getTime()) /
-        (1000 * 60 * 60 * 24);
-
-      status = `Voting opens in ${timeUntilVotingOpen.toFixed(0)} day${
-        timeUntilVotingOpen.toFixed(0) !== '1' ? 's' : ''
-      }`;
-    } else if (
-      today >= electionInfo.votingOpen &&
-      today < electionInfo.votingClose
-    ) {
-      status = 'Vote for our next execs';
-    } else {
-      status = 'Stay tuned for results';
-    }
-    setElectionStatus(status);
-  }, [today]);
-
-  const [buttonSize, setButtonSize] = useState<'sm' | 'md' | 'lg' | undefined>(
-    'lg',
-  );
-
-  const updateSize = () => {
-    if (window.innerWidth < 768) {
-      // 768px is the breakpoint for md in Tailwind CSS
-      setButtonSize('sm');
-    } else {
-      setButtonSize('lg');
-    }
-  };
-
-  useEffect(() => {
-    updateSize();
-    window.addEventListener('resize', updateSize);
-
-    // Cleanup function to remove the event listener when the component unmounts
-    return () => window.removeEventListener('resize', updateSize);
-  }, []);
+  // const bookingId = "6531a67db0a3f963db5b4175"
+  // const handleOpen = () => setOpen(true)
+  // const handleClose = () => setOpen(false)
 
   return (
-    <PageLayout
-      noBackground
-      transparentNav
-      className='bg-gradient-to-b from-[#420a1b] to-[#121212]'
-      darkMode
-    >
-      <main className='layout gap-y-16'>
-        <div className='flex flex-col items-center'>
-          <motion.section
-            id='hero-section'
-            className='mt-12 flex max-h-fit w-full max-w-[15rem] flex-col gap-x-8 gap-y-20 md:max-w-[30rem] lg:aspect-[5/2] lg:max-w-none lg:flex-row'
-            variants={containerMotionVariants}
-            initial='hidden'
-            animate='visible'
-          >
-            {/* Next MES President */}
-            <HeroSectionColumn
-              img='/images/homepage/alexis-moutafis-tymcio-faded.png'
-              classname='transition-all ease-in-out hover:flex-grow-[3] flex-grow-[1] duration-500 bg-gradient-to-br from-slate-50 to-slate-200'
+    <PageLayout noBackground>
+      <main className='layout'>
+        <section className='flex flex-col gap-10'>
+          <div className='space-y-9'>
+            <ButtonLink
+              href='/hatch-booking'
+              className='drop-shadow-2xl rounded-full bg-red-50 text-primary-700 transition-colors duration-75 pr-4'
+              size='sm'
+              leftIcon={ChevronLeftIcon}
             >
-              <div className='flex h-full w-full flex-col items-center justify-end text-center uppercase'>
-                <h1 className='text-3xl font-medium md:text-5xl'>2024/25</h1>
-                <div className='mt-6 flex flex-col text-xs font-medium md:text-sm'>
-                  <p>Our next MES President</p>
-                  <p className='font-bold'>Alexis Moutafis-Tymcio</p>
-                </div>
-              </div>
-            </HeroSectionColumn>
-
-            {/* SAGM */}
-            {/* <HeroSectionColumn
-              classname='transition-all ease-in-out hover:flex-grow-[3] flex-grow-[1] duration-500 bg-cover bg-center'
-              style={{
-                backgroundImage:
-                  "url('/images/sagm/winter-2024/sagm-winter-poster.png')",
-              }}
-            >
-              <div className='flex h-full w-full flex-col items-center justify-end px-12 pb-12 text-center text-white'>
-                <div className='rounded-lg bg-black/25 px-4 py-2'>
-                  <UnderlineLink href='https://docs.google.com/document/d/1QaIAWM7mOgybsFd9vRMkOPbnNJZivPf3EMqFYivPw-0/edit?usp=sharing'>
-                    Agenda
-                  </UnderlineLink>
-                </div>
-              </div>
-            </HeroSectionColumn> */}
-
-            {/* Appointed Positions */}
-            {/* <HeroSectionColumn
-              classname='transition-all ease-in-out hover:flex-grow-[3] flex-grow-[1] duration-500 bg-cover bg-center'
-              style={{
-                backgroundImage:
-                  "url('/images/homepage/appointed-positions-poster.png')",
-              }}
-            >
-              <div className='flex h-full w-full flex-row items-end justify-center gap-2 px-12 pb-6 text-center text-white sm:pb-16'>
-                <div className='rounded-lg bg-black/25 px-4 py-2'>
-                  <UnderlineLink href='https://docs.google.com/forms/d/e/1FAIpQLSc7xm0UmdFyNBIkI6KhrFicwFiBsswCiFXNhy6O24Slzei0dw/viewform'>
-                    Application
-                  </UnderlineLink>
-                </div>
-                <div className='rounded-lg bg-black/25 px-4 py-2'>
-                  <UnderlineLink href='https://docs.google.com/document/d/1NdPlxGh0xF0qYdg7peFhktVLaoPCEcMl-D7In0uRaXU/edit?usp=sharing'>
-                    Positions
-                  </UnderlineLink>
-                </div>
-              </div>
-            </HeroSectionColumn> */}
-
-            {/* Mac Eng Musical */}
-            {/* <HeroSectionColumn
-              img='/images/homepage/maceng-musical-faded.png'
-              classname='transition-all ease-in-out hover:flex-grow-[3] flex-grow-[1] duration-500 bg-gradient-to-br from-slate-50 to-slate-200'
-            >
-              <div className='flex h-full w-full flex-col items-center justify-end text-center'>
-                <div className='flex flex-row gap-x-1 text-sm font-medium uppercase'>
-                  <span className='flex'>Mac Eng Musical</span>
-                  <span className='hidden md:flex'>Presents</span>
-                </div>
-                <Image
-                  src='/images/homepage/mem-beetlejoules.png'
-                  alt='BeetleJoules Logo'
-                  width={500}
-                  height={346}
-                  className='md:w-2/3'
-                />
-                <span className='mt-6 text-[10px] leading-[14px] text-gray-600 md:text-sm'>
-                  March 14, 15, 16th @ 7pm
-                </span>
-                <div>
-                  <UnderlineLink href='https://www.eventbrite.ca/e/mcmaster-engineering-musical-presents-beetlejoules-tickets-828623955557?utm-campaign=social&utm-content=attendeeshare&utm-medium=discovery&utm-term=listing&utm-source=cp&aff=ebdsshcopyurl'>
-                    <p>Buy tickets today!</p>
-                  </UnderlineLink>
-                </div>
-              </div>
-            </HeroSectionColumn> */}
-
-            {/* Hatch Room Booking */}
-            <HeroSectionColumn
-              img='/images/homepage/gerald-hatch-centre-faded.png'
-              classname='transition-all ease-in-out hover:flex-grow-[3] flex-grow-[1] duration-500 bg-gradient-to-br from-slate-50 to-slate-200'
-            >
-              <div className='flex h-full w-full flex-col justify-end'>
-                <div className='flex flex-col uppercase'>
-                  <div className='flex flex-col'>
-                    <h1 className='text-3xl md:text-5xl'>Room</h1>
-                    <h1 className='text-3xl md:text-5xl'>Booking</h1>
-                  </div>
-                  <p className='mt-4 text-xs font-medium md:text-sm'>
-                    Gerald Hatch Centre
-                  </p>
-                </div>
-                <div className='mt-14'>
-                  <ArrowLink href='/bookings'>
-                    <p>Book now</p>
-                  </ArrowLink>
-                </div>
-              </div>
-            </HeroSectionColumn>
-
-            {/* MES Elections */}
-            <HeroSectionColumn
-              img='/images/homepage/mes-elections-faded.png'
-              imgLocation='bottom'
-              classname='transition-all ease-in-out hover:flex-grow-[3] flex-grow-[2] duration-500 bg-gradient-to-br from-[#ff2842] to-[#c70e24]'
-            >
-              <div className='flex h-full w-full flex-col justify-between'>
-                <div className='flex flex-col uppercase'>
-                  <p className='text-xs font-medium md:text-sm'>
-                    {electionStatus}
-                  </p>
-                  <h1 className='text-2xl md:text-5xl'>Rep & VP</h1>
-                  <h1 className='text-2xl md:text-5xl'>Elections</h1>
-                </div>
-                <Button
-                  radius='full'
-                  size={buttonSize}
-                  className='w-2/3 bg-black text-sm font-bold text-[#ff2842] sm:w-1/2'
-                  onPress={() => {
-                    window.location.href = '/elections';
-                  }}
+              Booking Page
+            </ButtonLink>
+            <div className='flex justify-start gap-8'>
+              <DashboardIconSvg />
+              <span className='hidden text-5xl font-bold md:block'>
+                Welcome to your Dashboard!
+              </span>
+            </div>
+          </div>
+          <div className='flex flex-col gap-10 sm:grid sm:grid-cols-3'>
+            <ActionCard
+              description={
+                <>
+                  Report an <strong>issue</strong>:
+                </>
+              }
+              button={
+                <ButtonLink
+                  href='https://docs.google.com/forms/d/e/1FAIpQLSfCu5qtc2_HmYWJfM7aYtO0jcDEoB6rAt9VXJx-Op0k_Gc-kQ/viewform'
+                  target='_blank'
+                  className='drop-shadow-2xl rounded-full bg-red-50 text-primary-700 transition-colors duration-75 cursor-pointer'
+                  size='sm'
+                  leftIcon={MessageCircleWarningIcon}
                 >
-                  <Link href='/elections'>Learn More</Link>
-                </Button>
-              </div>
-            </HeroSectionColumn>
-          </motion.section>
-        </div>
-        <motion.section
-          id='quick-links'
-          className='grid w-full grid-cols-2 gap-y-8 sm:flex sm:flex-row sm:justify-around sm:gap-x-8'
-          variants={containerMotionVariants}
-          initial='hidden'
-          animate='visible'
-        >
-          {quickLinks.map(({ icon: Icon, label, href }) => (
-            <Link
-              href={href}
-              key={label}
-              target={!href.startsWith('/') ? '_blank' : undefined}
-            >
-              <motion.div
-                id={`quick-link-${label.toLowerCase().replace(' ', '-')}`}
-                className='flex flex-col items-center gap-y-4 text-white'
-                variants={itemMotionVariants}
-                initial='hidden'
-                animate='visible'
-              >
-                <div className='quick-link-icon rounded-full bg-white p-4 text-black transition-all hover:scale-105 hover:bg-gray-300'>
-                  <Icon size={30} />
+                  Report Issue
+                </ButtonLink>
+              }
+            />
+            <ActionCard
+              description={
+                <div className='text-nowrap'>
+                  Email the <strong>hatch coordinator</strong>:
                 </div>
-                <span className='max-w-[100px] text-center text-sm'>
-                  {label}
-                </span>
-              </motion.div>
-            </Link>
-          ))}
-        </motion.section>
-        <section id='calendar'>
-          <Calendar apiKey={API_KEY} calendarIds={CALENDAR_IDS} darkMode />
+              }
+              button={
+                <ButtonLink
+                  href='mailto:ghc@mcmaster.ca'
+                  target='_blank'
+                  className='drop-shadow-2xl rounded-full bg-red-50 text-primary-700 transition-colors duration-75 text-nowrap cursor-pointer'
+                  leftIcon={MailIcon}
+                  size='sm'
+                >
+                  Email
+                </ButtonLink>
+              }
+            />
+            <ActionCard
+              description={
+                <>
+                  <strong>Rebook</strong> your <br /> next booking:
+                </>
+              }
+              button={
+                <ButtonLink
+                  href='https://mcmaster.tutorocean.com/search'
+                  target='_blank'
+                  className='drop-shadow-2xl rounded-full bg-red-50 text-primary-700 transition-colors duration-75 cursor-pointer'
+                  leftIcon={NotebookPenIcon}
+                  size='sm'
+                >
+                  Rebook booking
+                </ButtonLink>
+              }
+            />
+          </div>
+
+          <div className='grid grid-cols-2 gap-20'>
+            <UserInfo />
+            <div>
+              <NextBooking />
+              <UpcomingBookings />
+            </div>
+          </div>
+
+          <div className='p-10 flex justify-center w-full'>
+            <button
+              className='rounded-full py-1.5 px-3 bg-sky-500 hover:bg-blue-500 text-white'
+              onClick={() => setOpen(true)}
+            >
+              ···
+            </button>
+            <RebookModal
+              open={open}
+              onClose={() => setOpen(false)}
+              bookingId='6531a67db0a3f963db5b4175'
+            ></RebookModal>
+          </div>
         </section>
       </main>
     </PageLayout>
   );
-}
+};
+
+export default UserDashboard;
--- src/components/modals/TomorrowModal.tsx
+++ src/components/modals/TomorrowModal.tsx
@@ -4,15 +4,17 @@ import React, { useState } from 'react';
 import ButtonLink from '@/components/links/ButtonLink';
 
 type TomorrowModalProps = {
-  open: boolean
-  onClose: ()=> void
-  tomInfo: string
-}
+  open: boolean;
+  onClose: () => void;
+  tomInfo: string;
+};
 
-
-
-const TomorrowModal: React.FC<TomorrowModalProps> = ({open, onClose, tomInfo}) => {
-
+// eslint-disable-next-line unused-imports/no-unused-vars
+const TomorrowModal: React.FC<TomorrowModalProps> = ({
+  open,
+  onClose,
+  tomInfo,
+}) => {
   // const [roomsAvail, setRoomsAvail] = useState({
   //   h201: ["h201", true],
   //   h203: ["h203", true],
@@ -21,11 +23,11 @@ const TomorrowModal: React.FC<TomorrowModalProps> = ({open, onClose, tomInfo}) =
   //   h204b: ["h204b", true],
   // })
 
-  const h201Av = true
-  const h203Av = true
-  const h205Av = true
-  const h204aAv = false
-  const h204bAv = true
+  const h201Av = true;
+  const h203Av = true;
+  const h205Av = true;
+  const h204aAv = false;
+  const h204bAv = true;
 
   // const roomSel = useState({
   //   "h201": false,
@@ -51,106 +53,122 @@ const TomorrowModal: React.FC<TomorrowModalProps> = ({open, onClose, tomInfo}) =
   //   })
   // }
 
-  const roomButtonClass = (room: string, av:boolean) =>
-    av === true 
+  const roomButtonClass = (room: string, av: boolean) =>
+    av === true
       ? `border-solid border-1 rounded-lg py-0.5 px-1 mr-2.5 text-sm 
-        ${(selectedRoom===room)
-          ? "text-green-600 border-green-600 bg-green-100" 
-          : "bg-white border-gray-600 hover:bg-green-100 hover:text-green-600 hover:border-green-600"}`
+        ${
+          selectedRoom === room
+            ? 'text-green-600 border-green-600 bg-green-100'
+            : 'bg-white border-gray-600 hover:bg-green-100 hover:text-green-600 hover:border-green-600'
+        }`
       : `border-solid border-1 rounded-lg py-0.5 px-1 mr-2.5 text-sm bg-gray-50 border-gray-300 text-gray-300`;
-  
 
-
-    return (
-      <div 
-        className={`fixed inset-0 flex justify-center items-center transition-colors
-          ${open ? "visible bg-black/20" : "invisible"}`}
-        onClick={onClose}
+  return (
+    <div
+      className={`fixed inset-0 flex justify-center items-center transition-colors
+          ${open ? 'visible bg-black/20' : 'invisible'}`}
+      onClick={onClose}
+    >
+      <div
+        className={`bg-white rounded-lg shadow transition-all max-w-2xl border-solid border-2 border-green-300 py-8 px-12
+            ${open ? 'scale-100 opacity-100' : 'scale-110 opacity-0'}`}
+        onClick={(e) => e.stopPropagation()}
       >
-        <div
-          className={`bg-white rounded-lg shadow transition-all max-w-2xl border-solid border-2 border-green-300 py-8 px-12
-            ${open ? "scale-100 opacity-100" : "scale-110 opacity-0"}`}
-          onClick={(e) => e.stopPropagation()}
+        <button
+          className='absolute top-2 right-2 py-0.5 px-2 border 
+              berder-neutral-200 rounded-md text-gray-400 bg-white hover:bg-gray-50 hover:text-gray-600'
+          onClick={onClose}
         >
-          <button 
-            className="absolute top-2 right-2 py-0.5 px-2 border 
-              berder-neutral-200 rounded-md text-gray-400 bg-white hover:bg-gray-50 hover:text-gray-600"
-            onClick={onClose}
+          X
+        </button>
+        {/* {children} */}
+        <h1 className='text-center mx mb-5'>Same Time Tomorrow</h1>
+
+        <div className='flex'>
+          <button
+            className={`border-solid border-2 border-green-600 rounded-lg px-5 py-5 m-3 bg-green-100 text-green-600 "`}
           >
-            X
-          </button>
-          {/* {children} */}
-          <h1 className="text-center mx mb-5">Same Time Tomorrow</h1>
-
-          <div className="flex">
-            <button
-                className={`border-solid border-2 border-green-600 rounded-lg px-5 py-5 m-3 bg-green-100 text-green-600 "`}
-              >
-                <div>
-                  <b className='text-lg'>Same time<br/>tomorrow:<br/></b>
-                  <i>4:30pm (2H)<br/></i>
-                  <i>07/31/24<br/></i>
-                </div>
-              </button>
-            <div className="ml-5">
-
-              <h4 className="mt-2 mb-0.5">Available Rooms:</h4>
-              <div className="flex">
-
-                <button
-                  className={roomButtonClass("h201", h201Av)}
-                  onClick={() => {handleRoomSelection("h201")} 
-                  }
-                >
-                  H201
-                </button>
-
-                <button
-                  className={roomButtonClass("h203", h203Av)}
-                  onClick={() => {handleRoomSelection("h203")} 
-                  }
-                >
-                  H203
-                </button>
-
-                <button
-                  className={roomButtonClass("h205", h205Av)}
-                  onClick={() => {handleRoomSelection("h205")} 
-                  }
-                >
-                  H205
-                </button>
-                <button
-                  className={roomButtonClass("h204a", h204aAv)}
-                  onClick={() => {handleRoomSelection("h204a")} 
-                  }
-                >
-                  H204A
-                </button>
-                <button
-                  className={roomButtonClass("h204b", h204bAv)}
-                  onClick={() => {handleRoomSelection("h204b")} 
-                  }
-                >
-                  H204B
-                </button>
-              </div>
-              <h4 className="mt-3">Confirm Booking:</h4>
-              <p className="text-small mb-1.5">Room <b>H201</b>, <b>4:30 pm (2H), July 31, 2024. </b></p>
-              <ButtonLink
-                  href='/user-dashboard'
-                  className='drop-shadow-2xl rounded-full bg-red-50 text-primary-700 transition-colors duration-75 py-0.'
-                  size='sm'
-                  leftIcon={Bookmark}
-                  leftIconClassName='w-4 h-4'
-                >
-                  Book Now
-                </ButtonLink> 
+            <div>
+              <b className='text-lg'>
+                Same time
+                <br />
+                tomorrow:
+                <br />
+              </b>
+              <i>
+                4:30pm (2H)
+                <br />
+              </i>
+              <i>
+                07/31/24
+                <br />
+              </i>
             </div>
+          </button>
+          <div className='ml-5'>
+            <h4 className='mt-2 mb-0.5'>Available Rooms:</h4>
+            <div className='flex'>
+              <button
+                className={roomButtonClass('h201', h201Av)}
+                onClick={() => {
+                  handleRoomSelection('h201');
+                }}
+              >
+                H201
+              </button>
+
+              <button
+                className={roomButtonClass('h203', h203Av)}
+                onClick={() => {
+                  handleRoomSelection('h203');
+                }}
+              >
+                H203
+              </button>
+
+              <button
+                className={roomButtonClass('h205', h205Av)}
+                onClick={() => {
+                  handleRoomSelection('h205');
+                }}
+              >
+                H205
+              </button>
+              <button
+                className={roomButtonClass('h204a', h204aAv)}
+                onClick={() => {
+                  handleRoomSelection('h204a');
+                }}
+              >
+                H204A
+              </button>
+              <button
+                className={roomButtonClass('h204b', h204bAv)}
+                onClick={() => {
+                  handleRoomSelection('h204b');
+                }}
+              >
+                H204B
+              </button>
+            </div>
+            <h4 className='mt-3'>Confirm Booking:</h4>
+            <p className='text-small mb-1.5'>
+              Room <b>H201</b>, <b>4:30 pm (2H), July 31, 2024. </b>
+            </p>
+            <ButtonLink
+              href='/user-dashboard'
+              className='drop-shadow-2xl rounded-full bg-red-50 text-primary-700 transition-colors duration-75 py-0.'
+              size='sm'
+              leftIcon={Bookmark}
+              leftIconClassName='w-4 h-4'
+            >
+              Book Now
+            </ButtonLink>
           </div>
         </div>
       </div>
-    )
-}
+    </div>
+  );
+};
 
 export default TomorrowModal;
