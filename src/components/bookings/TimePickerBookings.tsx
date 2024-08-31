dfb2a78f71e00915d58f2546e5ac9f61adc8a2a0
Gary Qin <garyzhqin@gmail.com>
2024-08-30 22:07:51 -0400

    refactor: move booking indicators to BookingIndicator.tsx

--- src/components/bookings/TimePickerBookings.tsx
+++ src/components/bookings/TimePickerBookings.tsx
@@ -1,16 +1,12 @@
 'use client';
 
-import { Tooltip } from '@nextui-org/react';
 import { useEffect, useState } from 'react';
 
 import { useTimePickerContext } from '@/lib/context/TimePickerContext';
 
+import BookingIndicator from '@/components/bookings/BookingIndicator';
+
 import { TBooking } from '@/app/api/types';
-import {
-  AdminBookingIndicatorColours,
-  BookingIndicatorPositions,
-  UserBookingIndicatorColours,
-} from '@/constant/hatch-bookings/booking-indicator-data';
 
 /**
  * mock API call to fetch bookings
@@ -133,12 +129,6 @@ async function fetchAllBookings(): Promise<TBooking[]> {
   return mockBookings;
 }
 
-type Booking = {
-  room: string;
-  startTime: Date;
-  endTime: Date;
-};
-
 /**
  * @param isAdmin if true, display all existing bookings. if false, display only user's existing bookings
  * @param userBookings array of user's existing bookings
@@ -159,11 +149,11 @@ const TimePickerBookings = ({
   timeslotCount,
   firstTimeslot,
 }: TimePickerBookingsProps) => {
-  const [bookings, setBookings] = useState<Booking[]>([]);
+  const [bookings, setBookings] = useState<TBooking[]>([]);
   const { userBookings, roomVisibilities } = useTimePickerContext();
 
   useEffect(() => {
-    const visibleBookings: Booking[] = [];
+    const visibleBookings: TBooking[] = [];
     if (isAdmin) {
       fetchAllBookings().then((data) => {
         data.forEach((booking) => {
@@ -173,11 +163,7 @@ const TimePickerBookings = ({
           );
 
           if (timeslotDiv) {
-            visibleBookings.push({
-              room: booking.room,
-              startTime: new Date(booking.startTime),
-              endTime: new Date(booking.endTime),
-            });
+            visibleBookings.push(booking);
           }
         });
 
@@ -191,11 +177,7 @@ const TimePickerBookings = ({
         );
 
         if (timeslotDiv) {
-          visibleBookings.push({
-            room: booking.room,
-            startTime: new Date(booking.startTime),
-            endTime: new Date(booking.endTime),
-          });
+          visibleBookings.push(booking);
         }
       });
 
@@ -206,12 +188,8 @@ const TimePickerBookings = ({
   const TimePickerCell = ({ time }: { time: string }) => {
     // finds all the bookings at a certain time
     const bookingsAtTime = bookings.filter((booking) => {
-      return booking.startTime.toISOString() == time;
+      return new Date(booking.startTime).toISOString() == time;
     });
-
-    const getRoomIndicatorHeight = (start: Date, end: Date) => {
-      return `${((end.getTime() - start.getTime()) / 1800000) * 16 + 8}px`;
-    };
 
     return (
       <div id={time} className='relative flex w-full flex-1 justify-evenly'>
@@ -221,38 +199,12 @@ const TimePickerBookings = ({
                 return null;
               }
 
-              // substract 4 hours worth of milliseconds for EST
-              const formattedStartTime = new Date(
-                booking.startTime.getTime() - 14400000,
-              );
-              // substract 4 hours worth of milliseconds for EST
-              // add 30 minutes worth of milliseconds to make range include last timeslot
-              const formattedEndTime = new Date(
-                booking.endTime.getTime() - 14400000 + 1800000,
-              );
-              const bookingTooltipContent = `${booking.room}, ${formattedStartTime.toISOString().split('T')[1].substring(0, 5)}-${formattedEndTime.toISOString().split('T')[1].substring(0, 5)}`;
-
               return (
-                <div
+                <BookingIndicator
                   key={`${time}-${booking.room}`}
-                  className={`absolute top-1/3 flex items-center justify-center text-xs ${BookingIndicatorPositions[booking.room] || ''}`}
-                >
-                  <Tooltip
-                    showArrow
-                    content={bookingTooltipContent}
-                    placement='bottom'
-                  >
-                    <div
-                      style={{
-                        height: getRoomIndicatorHeight(
-                          booking.startTime,
-                          booking.endTime,
-                        ),
-                      }}
-                      className={`w-2 rounded-full ${(isAdmin ? AdminBookingIndicatorColours[booking.room] : UserBookingIndicatorColours[booking.room]) || 'bg-gray-500/70'} pointer-events-auto`}
-                    />
-                  </Tooltip>
-                </div>
+                  booking={booking}
+                  isAdmin={isAdmin}
+                />
               );
             })
           : null}
