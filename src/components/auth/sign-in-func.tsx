da61fc6e85e4d139a9ab6ea6f2359cd1cd78bbd5
HagOrMan <khagypi3@gmail.com>
2024-08-31 21:28:58 -0400

    fix: removed faulty auth components

--- src/components/auth/sign-in-func.tsx
+++
@@ -1,9 +0,0 @@
-'use server';
-import { signIn } from 'auth';
-
-// NOTE: DO NOT USE THIS. REDIRECTS TO `/auth/sign-in` SHOULD BE USED INSTEAD. KEEPING THIS TEMPORARILY IN CASE IT IS NEEDED IN THE NEAR FUTURE.
-
-// Uses Authjs signin. Since it accepts no parameters, this is just used to send them to the sign-in page.
-export default async function signInComponent() {
-  await signIn();
-}
