f49ff9a87c7a8fab2e773d7318de5a6535022701
Owen Gretzinger <owengretzinger@gmail.com>
2024-09-01 00:59:54 -0400

    fix: improve date selector font size

--- src/components/bookings/TimePickerChanger.tsx
+++ src/components/bookings/TimePickerChanger.tsx
@@ -28,7 +28,7 @@ const TimePickerChanger = () => {
       <Button onClick={handleShiftBackward}>
         <ArrowLeft />
       </Button>
-      <h2 className='font-semibold md:text-xl'>
+      <h2 className='text-base font-semibold lg:text-xl'>
         {/* on mobile show 1 day (ex. "August 31st") */}
         {/* on tablet/laptop show range (ex. "August 31st - September 6th") */}
         {format(pickerStartDate, 'MMMM do')}
