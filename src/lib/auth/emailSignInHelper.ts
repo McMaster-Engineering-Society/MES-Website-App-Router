0c0e70d9193b1f10a87685726224ee084191823a
HagOrMan <khagypi3@gmail.com>
2024-08-31 22:13:52 -0400

    feat: add function go get email of signed in user

--- src/lib/auth/emailSignInHelper.ts
+++ src/lib/auth/emailSignInHelper.ts
@@ -2,6 +2,15 @@
 
 import { auth, signIn } from 'auth';
 
+/**
+ * Finds user's email if the user is authenticated (has a valid and active session in the database).
+ * @returns email address if user is authenticated already, null if not.
+ */
+export const getUserEmail = async () => {
+  const session = await auth();
+  return session?.user?.email ?? null;
+};
+
 export const checkIsAuthenticated = async () => {
   const session = await auth();
   // eslint-disable-next-line no-console
