3ff9a882d14a7b4776f46c7d3f7432236cdedf81
TheLime01 <emily134luu@gmail.com>
2024-09-02 05:32:52 -0400

    feat: dummy file to represent UHS forms

---
+++ src/components/admin-portal/UHSForms.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UHSForms = () => {
+  return (
+    <div className='flex items-center px-7 h-280 w-230 bg-gray border-2 rounded-2xl'>
+      <h2>FORM</h2>
+    </div>
+  );
+};
+
+export default UHSForms;
