1cb35917954e94f64d691e7da2c75591d2e926ce
TheLime01 <emily134luu@gmail.com>
2024-09-02 05:36:57 -0400

    feat: admin sidebar icons and headers

---
+++ src/constant/clubs-dashboard/AdminSidebarItems.ts
@@ -0,0 +1,49 @@
+'use client';
+import { FaHouse } from 'react-icons/fa6';
+import { FaScroll } from 'react-icons/fa6';
+import { FaSquareCheck } from 'react-icons/fa6';
+import { FaDoorOpen } from 'react-icons/fa6';
+import { FaNewspaper } from 'react-icons/fa6';
+import { FaCalendarDays } from 'react-icons/fa6';
+import { FaPeopleGroup } from 'react-icons/fa6';
+
+import SidebarItem from '@/types/clubs-dashboard/SidebarItemType';
+
+const url = '/admin-dashboard';
+export const sidebarItems: SidebarItem[] = [
+  {
+    name: 'Admin Dashboard',
+    icon: FaHouse,
+    link: `${url}`,
+  },
+  {
+    name: 'Documentation',
+    icon: FaScroll,
+    link: `${url}/expenses`,
+  },
+  {
+    name: 'Add/Remove Club',
+    icon: FaSquareCheck,
+    link: `${url}/events-approval`,
+  },
+  {
+    name: 'View all Clubs',
+    icon: FaPeopleGroup,
+    link: `${url}/room-booking`,
+  },
+  {
+    name: 'View all Events',
+    icon: FaCalendarDays,
+    link: `${url}/rentals-and-services`,
+  },
+  {
+    name: 'UHS Forms',
+    icon: FaNewspaper,
+    link: `${url}/help`,
+  },
+  {
+    name: 'Room Bookings',
+    icon: FaDoorOpen,
+    link: `${url}/club-administration`,
+  },
+];
