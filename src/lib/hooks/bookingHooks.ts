b6aa81c572f21ea596ecad516df412645918b54d
HagOrMan <khagypi3@gmail.com>
2024-09-11 22:13:38 -0400

    fix: session context and profile loading errors with react hooks

--- src/lib/hooks/bookingHooks.ts
+++ src/lib/hooks/bookingHooks.ts
@@ -64,7 +64,11 @@ export const useDeleteBookingHook = () => {
   });
 };
 
-export const useFetchAllBookingsHook = (startDate: Date, endDate: Date) => {
+export const useFetchAllBookingsHook = (
+  startDate: Date,
+  endDate: Date,
+  enabled: boolean,
+) => {
   return useQuery<TBooking[], Error>({
     queryKey: [
       'allBookings',
@@ -75,5 +79,6 @@ export const useFetchAllBookingsHook = (startDate: Date, endDate: Date) => {
     staleTime: 1000 * 60 * 5, // 5 minutes
     refetchOnWindowFocus: false,
     refetchOnMount: false,
+    enabled: enabled,
   });
 };
