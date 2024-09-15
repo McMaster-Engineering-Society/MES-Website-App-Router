038e71e1c89d97dd8449d69a255981684074a41c
BaoGeist <linofbaoze@outlook.com>
2024-09-15 16:28:35 -0400

    fix: updated env example and fixed display in auth/error page

--- src/app/auth/error/page.tsx
+++ src/app/auth/error/page.tsx
@@ -1,5 +1,4 @@
 import ServerSignInGate from '@slices/auth/components/ServerSignInGate';
-import SignInGatePage from '@slices/auth/components/SignInGatePage';
 import React from 'react';
 
 import { ErrorPage } from '@/app/auth/error/ErrorPage';
@@ -9,11 +8,7 @@ const href = '/';
 
 const Error = async () => {
   await ServerSignInGate({ requireSignIn: requireSignIn, href: href });
-  return (
-    <SignInGatePage requireSignIn={requireSignIn} href={href}>
-      <ErrorPage />
-    </SignInGatePage>
-  );
+  return <ErrorPage />;
 };
 
 export default Error;
