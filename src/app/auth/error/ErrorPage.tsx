07911da0afbbcb8d1e9b64ae58ff9d39d8b0d508
Arsia Khorramijam <145126771+arsiakh@users.noreply.github.com>
2024-09-29 20:26:33 -0400

    feat: new error and success page (#326)

--- src/app/auth/error/ErrorPage.tsx
+++ src/app/auth/error/ErrorPage.tsx
@@ -1,52 +1,52 @@
 'use client';
 
-import { SignOut } from '@slices/auth/components/SignOutButton';
 import { useSearchParams } from 'next/navigation';
-// import { redirect } from 'next/navigation';
+import { redirect } from 'next/navigation';
 import * as React from 'react';
 import { RiAlarmWarningFill } from 'react-icons/ri';
 
 import PageLayout from '@/components/layout/PageLayout';
-import ButtonLink from '@/components/links/ButtonLink';
 
-// TODO: reformat page, make prettier
 export function ErrorPage() {
   const searchParams = useSearchParams();
   const error = searchParams.get('error');
 
   // Exits the page if we didn't get an error to lead us here from NextAuth.
   if (!error) {
-    // redirect('/auth/sign-in');
-    // eslint-disable-next-line no-console
-    console.log('no error apparently');
+    redirect('/auth/sign-in');
   }
   // eslint-disable-next-line no-console
   console.log(error);
   return (
     <PageLayout>
       <main className='layout'>
-        <section className='bg-white'>
-          <div className='layout flex min-h-screen flex-col items-center justify-center text-center text-black'>
-            <RiAlarmWarningFill
-              size={60}
-              className='drop-shadow-glow animate-flicker text-red-500'
-            />
-            <h1>
-              Oops! It looks like there was an error with signing in.
-              {error == 'Configuration' &&
-                ' You must use a McMaster email address in order to sign in.'}
-            </h1>
-            <ButtonLink
-              href='/auth/sign-in'
-              variant='outline'
-              className='my-8 w-1/4'
-            >
-              Try Again
-            </ButtonLink>
-            <h1>
-              If you're not sure why you got the error, try signing out here:
-            </h1>
-            <SignOut className='w-60 mt-4'></SignOut>
+        <section>
+          <div className='flex flex-col items-center min-h-screen'>
+            {' '}
+            {/*  bg-[hsl(0,0%,97.5%)]*/}
+            <div className='flex flex-col w-3/4 h-full rounded-t-lg shadow-md bg-white text-center items-center justify-center '>
+              <div className='flex flex-col items-center text-[hsl(0,45%,28%)] bg-red-400 rounded-t-lg w-full pb-14'>
+                <h1 className='flex flex-col items-center justify-center mt-10'>
+                  <RiAlarmWarningFill
+                    size={60}
+                    className='drop-shadow-glow animate-flicker text-red-500'
+                  />
+                </h1>
+                <h1 className='mt-5 mb-5'> Oops! </h1>
+                <h2 className='w-1/2 mb-5'>
+                  {' '}
+                  It looks like there was an error with signing in.{' '}
+                </h2>
+              </div>
+              <div className='w-5/6 p-8 rounded-lg items-center justify-center shadow-md bg-amber-200 text-center m-10'>
+                <div>
+                  <b>
+                    If you're not sure why you got the error, contact the
+                    administrator at ghc@mcmaster.ca
+                  </b>
+                </div>
+              </div>
+            </div>
           </div>
         </section>
       </main>
