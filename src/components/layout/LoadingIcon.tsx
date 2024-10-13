169a7a09e1336de81181424f19a4d3dd22b82ef2
HagOrMan <khagypi3@gmail.com>
2024-10-13 14:47:56 -0400

    fix: next env new text with upgraded version, centered text more

--- src/components/layout/LoadingIcon.tsx
+++ src/components/layout/LoadingIcon.tsx
@@ -8,7 +8,7 @@ export default function LoadingIcon({ text = 'Loading...' }: LoadingIconProps) {
   return (
     <div className='h-full flex flex-col items-center justify-center'>
       <div className='border-gray-300 h-10 w-10 animate-spin rounded-full border-8 border-t-red-800' />
-      {text && <div className='mt-5 font-bold'> {text}</div>}
+      {text && <div className='mt-5 font-bold text-center'> {text}</div>}
     </div>
   );
 }
