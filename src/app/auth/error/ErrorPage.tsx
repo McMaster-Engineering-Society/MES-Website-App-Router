87d08c2a4bd9d6e20143274ff8baccdf6f40ecc9
HagOrMan <khagypi3@gmail.com>
2024-10-13 23:05:33 -0400

    fix: bit more information on auth error page plus button to try again

--- src/app/auth/error/ErrorPage.tsx
+++ src/app/auth/error/ErrorPage.tsx
@@ -6,6 +6,7 @@ import * as React from 'react';
 import { RiAlarmWarningFill } from 'react-icons/ri';
 
 import PageLayout from '@/components/layout/PageLayout';
+import ButtonLink from '@/components/links/ButtonLink';
 
 export function ErrorPage() {
   const searchParams = useSearchParams();
@@ -15,16 +16,15 @@ export function ErrorPage() {
   if (!error) {
     redirect('/auth/sign-in');
   }
+
   // eslint-disable-next-line no-console
-  console.log(error);
+  console.log('Someone had an error signing in:', error);
   return (
     <PageLayout>
       <main className='layout'>
         <section>
-          <div className='flex flex-col items-center min-h-screen'>
-            {' '}
-            {/*  bg-[hsl(0,0%,97.5%)]*/}
-            <div className='flex flex-col w-3/4 h-full rounded-t-lg shadow-md bg-white text-center items-center justify-center '>
+          <div className='flex flex-col items-center min-h-full'>
+            <div className='flex flex-col w-3/4 rounded-t-lg shadow-md bg-white text-center items-center justify-center '>
               <div className='flex flex-col items-center text-[hsl(0,45%,28%)] bg-red-400 rounded-t-lg w-full pb-14'>
                 <h1 className='flex flex-col items-center justify-center mt-10'>
                   <RiAlarmWarningFill
@@ -34,17 +34,25 @@ export function ErrorPage() {
                 </h1>
                 <h1 className='mt-5 mb-5'> Oops! </h1>
                 <h2 className='w-1/2 mb-5'>
-                  {' '}
-                  It looks like there was an error with signing in.{' '}
+                  It looks like there was an error with signing in.
                 </h2>
               </div>
-              <div className='w-5/6 p-8 rounded-lg items-center justify-center shadow-md bg-amber-200 text-center m-10'>
-                <div>
-                  <b>
-                    If you're not sure why you got the error, contact the
-                    administrator at ghc@mcmaster.ca
-                  </b>
-                </div>
+              <div className='flex flex-col w-5/6 pt-6 pb-4 m-8 rounded-lg items-center justify-center text-center shadow-md bg-amber-200'>
+                <b>
+                  Sign in again, then copy and paste the sign-in link into your
+                  browser
+                </b>
+                <ButtonLink
+                  variant='outline'
+                  href='/auth/sign-in'
+                  className='mt-2 w-1/4'
+                >
+                  Try Again
+                </ButtonLink>
+              </div>
+              <div className='w-5/6 justify-center text-center mb-8'>
+                If the error persists, contact the administrator at
+                ghc@mcmaster.ca
               </div>
             </div>
           </div>
