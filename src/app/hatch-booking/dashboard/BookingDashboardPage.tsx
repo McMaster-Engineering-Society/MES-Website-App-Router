2273041ad49231329838549192f79af181349e27
BaoGeist <linofbaoze@outlook.com>
2024-10-17 14:52:44 -0400

    feat: add booking pagination

--- src/app/hatch-booking/dashboard/BookingDashboardPage.tsx
+++ src/app/hatch-booking/dashboard/BookingDashboardPage.tsx
@@ -13,28 +13,42 @@ import {
   MessageCircleWarningIcon,
   UserRoundCogIcon,
 } from 'lucide-react';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 import { cn } from '@/lib/utils';
 
+import LoadingIcon from '@/components/layout/LoadingIcon';
 import ButtonLink from '@/components/links/ButtonLink';
 import PageSection from '@/components/PageSection';
 
 import ProfilePicture from '@/constant/user-dashboard/ProfilePictureSvg';
 import { useSessionContext } from '@/slices/auth/context/SessionContext';
 import { BookingTimeslot } from '@/slices/hatch/booking-page/components/BookingTimeslot';
+import { BookingTimeslotPagination } from '@/slices/hatch/booking-page/components/BookingTimeslotPagination';
 import EditButton from '@/slices/hatch/booking-page/components/buttons/EditButton';
 import EditProfileModal from '@/slices/hatch/booking-page/components/modals/EditProfileModal';
 import ExpandModal from '@/slices/hatch/booking-page/components/modals/ExpandModal';
+import {
+  useNextBookings,
+  usePastBookings,
+} from '@/slices/hatch/booking-page/hooks/bookingHooks';
 
 const queryClient = new QueryClient();
 
 const UserDashboard = () => {
   const { profile } = useSessionContext();
 
-  const [nextBookingsData, setNextBookingsData] = useState<TBooking[]>([]);
-  const [pastBookingsData, setPastBookingsData] = useState<TBooking[]>([]);
-  const [nextBooking, setNextBooking] = useState<TBooking | null>(null);
+  const [nextBookingsPage, setNextBookingsPage] = useState<number>(1);
+  const [pastBookingsPage, setPastBookingsPage] = useState<number>(1);
+  const { data: nextBookingsData } = useNextBookings(
+    profile?.email,
+    nextBookingsPage,
+  );
+  const { data: pastBookingsData } = usePastBookings(
+    profile?.email,
+    pastBookingsPage,
+  );
+  const [nextBooking] = useState<TBooking | null>(null);
   const [open, setOpen] = useState<boolean>(false);
 
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
@@ -46,84 +60,6 @@ const UserDashboard = () => {
   const [displayId, setDisplayId] = useState<string>('');
 
   // todo: display a set number of past bookings and upcoming bookings? e.g: only show 5 of the past bookings, or have some sort of filtering / pagination in the future?
-
-  const fetchPastBookingsByEmail = async (email: string) => {
-    // To get the past bookings, use the range where start date is a year before today's date and end date is today
-    const startDate = new Date(
-      new Date().setFullYear(new Date().getFullYear() - 1),
-    );
-    const endDate = new Date();
-
-    const startDateISO = startDate.toISOString().slice(0, -1);
-    const endDateISO = endDate.toISOString().slice(0, -1);
-    const response = await fetch(
-      `/api/bookings/get-bookings-in-date-range-and-email?startdate=${startDateISO}&enddate=${endDateISO}&email=${email}`,
-      {
-        method: 'GET',
-      },
-    );
-    const jsonResponse = await response.json();
-
-    setPastBookingsData(() => {
-      const newPastBookings: TBooking[] = jsonResponse.data.map(
-        (booking: TBooking) => ({
-          _id: booking._id,
-          userId: booking.userId,
-          room: booking.room,
-          email: booking.email,
-          startTime: booking.startTime,
-          endTime: booking.endTime,
-          hasConfirmed: booking.hasConfirmed,
-          createdDate: booking.createdDate,
-        }),
-      );
-
-      return newPastBookings;
-    });
-  };
-
-  const fetchNextBookingsByEmail = async (email: string) => {
-    // To get the upcoming bookings, use the range where start date is today and end date is a year from now
-    const startDate = new Date();
-    const endDate = new Date(
-      new Date().setFullYear(new Date().getFullYear() + 1),
-    );
-    const startDateISO = startDate.toISOString().slice(0, -1);
-    const endDateISO = endDate.toISOString().slice(0, -1);
-    const response = await fetch(
-      `/api/bookings/get-bookings-in-date-range-and-email?startdate=${startDateISO}&enddate=${endDateISO}&email=${email}`,
-      {
-        method: 'GET',
-      },
-    );
-    const jsonResponse = await response.json();
-
-    setNextBooking(jsonResponse.data.shift());
-
-    setNextBookingsData(() => {
-      const newBookings: TBooking[] = jsonResponse.data.map(
-        (booking: TBooking) => ({
-          _id: booking._id,
-          userId: booking.userId,
-          room: booking.room,
-          email: booking.email,
-          startTime: booking.startTime,
-          endTime: booking.endTime,
-          hasConfirmed: booking.hasConfirmed,
-          createdDate: booking.createdDate,
-        }),
-      );
-
-      return newBookings;
-    });
-  };
-
-  useEffect(() => {
-    if (profile) {
-      fetchNextBookingsByEmail(profile.email);
-      fetchPastBookingsByEmail(profile.email);
-    }
-  }, [profile]);
 
   function handleExpand(
     startTime: Date,
@@ -192,9 +128,16 @@ const UserDashboard = () => {
               leftIcon={CalendarDaysIcon}
               className='rounded-lg'
             >
-              {pastBookingsData.length > 0 ? (
+              <BookingTimeslotPagination
+                page={pastBookingsPage}
+                setPage={setPastBookingsPage}
+                // eslint-disable-next-line @typescript-eslint/no-non-null-assertion, @typescript-eslint/no-non-null-asserted-optional-chain
+                totalPages={pastBookingsData?.totalCount!}
+              />
+              {pastBookingsData &&
+              pastBookingsData.newPastBookings.length > 0 ? (
                 <div className='flex flex-col gap-8 min-h-[75px]'>
-                  {pastBookingsData.map((booking, index) => {
+                  {pastBookingsData.newPastBookings.map((booking, index) => {
                     return (
                       <BookingTimeslot
                         key={index}
@@ -217,9 +160,7 @@ const UserDashboard = () => {
                   })}
                 </div>
               ) : (
-                <div className='flex flex-row items-center justify-center min-h-[75px]'>
-                  <p>You do not have any past bookings.</p>
-                </div>
+                <LoadingIcon />
               )}
             </PageSection>
           </div>
@@ -281,9 +222,15 @@ const UserDashboard = () => {
               leftIcon={CalendarDaysIcon}
               className='rounded-lg'
             >
-              {nextBookingsData.length > 0 ? (
+              <BookingTimeslotPagination
+                page={nextBookingsPage}
+                setPage={setNextBookingsPage}
+                // eslint-disable-next-line @typescript-eslint/no-non-null-assertion, @typescript-eslint/no-non-null-asserted-optional-chain
+                totalPages={nextBookingsData?.totalCount!}
+              />
+              {nextBookingsData && nextBookingsData.newBookings.length > 0 ? (
                 <div className='flex flex-col gap-8'>
-                  {nextBookingsData.map((booking, index) => {
+                  {nextBookingsData.newBookings.map((booking, index) => {
                     return (
                       <BookingTimeslot
                         key={index}
@@ -306,9 +253,7 @@ const UserDashboard = () => {
                   })}
                 </div>
               ) : (
-                <div className='flex flex-row items-center justify-center min-h-[75px]'>
-                  <p>You do not have any upcoming bookings.</p>
-                </div>
+                <LoadingIcon />
               )}
             </PageSection>
           </div>
