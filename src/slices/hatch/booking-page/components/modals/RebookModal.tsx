4465fd1b36ff678d65448ffb29d00c94723e1931
HagOrMan <khagypi3@gmail.com>
2024-10-19 17:16:11 -0400

    fix: edit booking modal fixed position, rebook length display

--- src/slices/hatch/booking-page/components/modals/RebookModal.tsx
+++ src/slices/hatch/booking-page/components/modals/RebookModal.tsx
@@ -11,6 +11,7 @@ import React, { useEffect, useState } from 'react';
 import TomorrowModal from '@/slices/hatch/booking-page/components/modals/TomorrowModal';
 import WeekModal from '@/slices/hatch/booking-page/components/modals/WeekModal';
 import { useFetchAvailabilitiesHook } from '@/slices/hatch/booking-page/hooks/bookingHooks';
+import { add30Minutes } from '@/slices/hatch/booking-page/utils';
 
 type RebookModalProps = {
   open: boolean;
@@ -43,7 +44,7 @@ const RebookModal: React.FC<RebookModalProps> = ({
   const [nextWeekOpen, setNextOpen] = useState<boolean>(false);
 
   function getDuration(startTime: Date, endTime: Date) {
-    const timeMin = differenceInMinutes(endTime, startTime);
+    const timeMin = differenceInMinutes(add30Minutes(endTime), startTime);
     const hours = Math.floor(timeMin / 60);
     const min = timeMin % 60;
     if (min != 0) {
