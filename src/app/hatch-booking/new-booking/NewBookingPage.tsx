e7f129ea36c06c63c6e91457ffa8b1020ee515c9
HagOrMan <khagypi3@gmail.com>
2024-10-19 17:42:05 -0400

    feat: add banners about it being a new system and wanting feedback

--- src/app/hatch-booking/new-booking/NewBookingPage.tsx
+++ src/app/hatch-booking/new-booking/NewBookingPage.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import Alert from '@mui/material/Alert';
 import React from 'react';
 
 import { useScreenSize } from '@/components/hooks/useScreenSize';
@@ -12,31 +13,41 @@ export default function BookingPage() {
   const screenSize = useScreenSize();
 
   return (
-    <div className='mb-8 flex w-full flex-col gap-8'>
-      <div className='flex flex-col gap-4 md:hidden'>
-        <TimePickerChanger />
-        <div className='grid h-[600px] grid-cols-2 gap-4'>
-          <TimePicker
-            numDaysToShow={
-              screenSize === 'lg' ? 7 : screenSize === 'md' ? 3 : 1
-            }
-          />
-          <AvailableRooms />
-        </div>
-      </div>
-
-      <div className='hidden gap-4 md:flex'>
-        <div className='flex flex-col gap-2'>
+    <>
+      <Alert className='h-[70px] sm:h-[50px]' severity='info'>
+        Welcome to the new booking system! Please click
+        <a href='https://forms.gle/fMPTNg1EdLBfthXW7'> here</a> to leave your
+        feedback.
+      </Alert>
+      <div className='mb-8 flex w-full flex-col gap-8'>
+        <div className='flex flex-col gap-4 md:hidden'>
           <TimePickerChanger />
-          <TimePicker
-            numDaysToShow={
-              screenSize === 'lg' ? 7 : screenSize === 'md' ? 3 : 1
-            }
-            className='h-full'
-          />
+          <div className='grid h-[600px] grid-cols-2 gap-4'>
+            <TimePicker
+              numDaysToShow={
+                screenSize === 'lg' ? 7 : screenSize === 'md' ? 3 : 1
+              }
+            />
+            <AvailableRooms />
+          </div>
         </div>
-        <AvailableRooms className='h-[600px]' />
+
+        <div className='hidden gap-4 md:flex'>
+          <div className='flex flex-col gap-2'>
+            <TimePickerChanger />
+            <TimePicker
+              numDaysToShow={
+                screenSize === 'lg' ? 7 : screenSize === 'md' ? 3 : 1
+              }
+              className='h-full'
+            />
+          </div>
+          <AvailableRooms className='h-[600px]' />
+        </div>
       </div>
-    </div>
+      <Alert className='h-[70px] sm:h-[50px]' severity='info'>
+        This is proudly developed by the MES's Infrastructure Technology Team
+      </Alert>
+    </>
   );
 }
