b72c080977c482668d8e57028c3b8ebd006ec57b
Kyle <86536365+HagOrMan@users.noreply.github.com>
2024-10-24 18:44:18 -0400

    fix: add next public url to auth callback, fix wording on auth pages, improve styling (#404)

--- src/app/booking-system/BookingSystemPage.tsx
+++ src/app/booking-system/BookingSystemPage.tsx
@@ -8,27 +8,30 @@ export default function BookingPage() {
   return (
     <PageLayout noFooter>
       <>
-        <Alert className='h-[70px] sm:h-[50px]' severity='info'>
+        <Alert className='h-auto' severity='info'>
           We're planning to release our new booking system soon, but we need
           your help to test it out and make sure it's ready! If you have a few
-          minutes to spare, please check it out{' '}
-          <Link
-            href='https://macengsociety.ca/hatch-booking/new-booking'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            here
-          </Link>{' '}
-          and let us know of any issues or suggestions you have at this
-          <Link
-            href='https://forms.gle/dbZq1rAYVgANM9Zr8'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            {' '}
-            form
-          </Link>
-          !
+          minutes to spare,{' '}
+          <span className='underline'>
+            <Link
+              href='https://www.macengsociety.ca/auth/sign-in'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              please check it out here
+            </Link>
+          </span>{' '}
+          and let us know of any issues or suggestions you have at{' '}
+          <span className='underline'>
+            <Link
+              href='https://forms.gle/dbZq1rAYVgANM9Zr8'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              this form
+            </Link>
+            !
+          </span>
         </Alert>
 
         <iframe
